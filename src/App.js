837ecb2a09f81ff6a0e3294ec03a4824e4f16c8e
Brian <briantrinh618@gmail.com>
2025-02-03 14:21:18 -0800

    Initial commit with most functions/features completed

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react'
+import WeatherCodeTable from './WeatherCode.js'
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [zipCode, setZipCode] = useState("")
+  const [zip, setZip] = useState(null)
+
+  const handleChange = ({target}) => setZipCode(target.value)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setZip(zipCode)
+  };
+
+  const [temperature, setTemperature] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [weatherCode, setWeatherCode] = useState(null)
+
+  const options = {
+    method: 'GET',
+    headers: {accept: 'application/json', 'accept-encoding': 'deflate, gzip, br'}
+  };
+
+  const apikey = "bI038WXUmkPN20prbxcAnrObUac6RqKm" // cannot securely store this in a client. need a backend server https://stackoverflow.com/questions/48699820/how-do-i-hide-an-api-key-in-create-react-app
+
+  useEffect( () => {
+    if (zip !== null) {
+      fetch(`https://api.tomorrow.io/v4/weather/realtime?location=${zipCode}%US&apikey=${apikey}`, options)
+        .then(res => res.json())
+        .then(res => {
+          console.log(res)
+          //setTemperature(res.data.values.temperature * 9 / 5 + 32)
+          setTemperature(res.data.values.temperature)
+          setWeatherCode(WeatherCodeTable[res.data.values.weatherCode])
+          setLocation(res.location.name)
+        })
+        .catch(err => console.error(err));
+    }
+  }, [zip]);
+
+  return (
+    <div class="outer-container">
+      <title> US Weather App </title>
+      <div class="title-container">
+        <h3> US Weather App </h3>
+      </div>
+      <form onSubmit = {handleSubmit}>
+        <input type="text" placeholder="Enter US Zip Code" value={zipCode} onChange={handleChange}></input>
+        <button type="submit"> Submit </button>
+      </form>
+      <div class="location-container">
+        {location && <h1 class="location-header"> {location} </h1> }
+      </div>
+      <div class="data-container">
+        {temperature && <h1> {temperature + "°C / " + Math.round( (temperature * 9 / 5 + 32) * 10)/10 + "°F"} </h1>}
+        {weatherCode && <h1> {weatherCode} </h1>}
+        {weatherCode && <h1> Include Picture </h1>}
+      </div>
+    </div>
+  );
+}
+
+export default App;
